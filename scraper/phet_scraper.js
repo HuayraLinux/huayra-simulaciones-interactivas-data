07edb280ac6b286c720c2bd716fce9443be92561
Ignacio Losiggio <iglosiggio@gmail.com>
2017-05-17 15:23:17 -0300

    El scraper ahora descarga las fotos y experimentos!

--- scraper/phet_scraper.js
+++ scraper/phet_scraper.js
@@ -1,32 +1,20 @@
 const osmosis = require('osmosis');
 const fs = require('fs');
+const {get} = require('https');
+const path = require('path').parse;
 
 const dump_filename = 'phet_scraper.json';
 
 function all_data(osmosis_instance) {
   const data_list = [];
-  return new Promise((resolve) =>
-    osmosis_instance.data((data) => data_list.push(data))
-                    .done(() => resolve(data_list)));
-}
-
-function unificar_experimentos([descargas, metadata]) {
-  /* Creo un objeto { nombre => experimento }  */
-  const experimentos_obj = descargas.reduce((obj, experimento) => {
-    return Object.assign({[experimento.nombre]: experimento}, obj)
-  }, {});
-
-  /* Le agrego la data */
-  const result = metadata.reduce((obj, data) => {
-    const experimento = Object.assign({}, data, experimentos_obj[data.nombre]);
-    return Object.assign({}, obj, { [data.nombre]: experimento });
-  }, {});
-
-  return result;
+  return new Promise(resolve => osmosis_instance
+    .data(data => data_list.push(data))
+    .done(() => resolve(data_list)));
 }
 
 function print(osmosis_instance) {
-  all_data(osmosis_instance).then(console.log);
+  all_data(osmosis_instance)
+  .then(console.log);
 }
 
 function write_json(data, filename = dump_filename, spaces = 4, cb = () => {}) {
@@ -37,30 +25,57 @@ function log(prefijo) {
   return console.log.bind(console, `[${prefijo}]`);
 }
 
-const experimentos_descargas_url = 'https://phet.colorado.edu/es/offline-access';
-const experimentos_descargas = osmosis(experimentos_descargas_url)
-.find('#offline-access tr.offline-access')
-.set({
-    'nombre': '.oa-title > span',
-    'html5': '.oa-html5 > a@href',
-    'flash': '.oa-flash > a@href',
-    'java': '.oa-java > a@href'
-})
-.log(log('experimentos'))
-.error(log('experimentos'));
+function download_file(url, filename) {
+  /* debug log */
+  console.log('[download_file]', url, filename, 'STARTED');
 
-const experimentos_data_url = 'https://phet.colorado.edu/es/simulations'
-const experimentos_data = osmosis(experimentos_data_url)
+  const file = fs.createWriteStream(filename);
+  return new Promise((resolve, reject) => get(url, res => (res.on('close', resolve), res.pipe(file))))
+    .then(use(() => console.log('[download_file]', url, filename, 'FINISHED')));
+}
+
+function mkdir(dir) {
+  return new Promise((resolve, reject) =>
+    fs.mkdir(dir, err => err? reject(err) : resolve()));
+}
+
+function create_dirs() {
+  return Promise.all([
+    mkdir('imagenes'),
+    mkdir('experimentos')
+  ]);
+}
+
+function download_data(experimentos) {
+  return Promise.all(experimentos.map(({imagen, descarga}) => {
+    const url = 'https://phet.colorado.edu';
+    const imagen_filename = path(imagen).base;
+    const descarga_filename = path(descarga).base.replace(/\?[^?]*$/, '');
+
+    return Promise.all([
+      download_file(`${url}${imagen}`, `imagenes/${imagen_filename}`),
+      download_file(`${url}${descarga}`, `experimentos/${descarga_filename}`)
+    ]);
+  }));
+}
+
+function use(fun) {
+  return val => fun(val).then(() => val);
+}
+
+const experimentos_url = 'https://phet.colorado.edu/es/simulations'
+const experimentos = osmosis(experimentos_url)
 .follow('a.simulation-link@href')
 .set({
   'nombre': '.simulation-main-title',
   'imagen': '.simulation-main-screenshot@src',
+  'descarga': '#sim-download@href',
   'categorias': ['.nml-link-label.selected'],
   'info': '#about:html',
   'credits': '#credits:html'
 })
-.log(log('experimentos_data'))
-.error(log('experimentos_data'));
+.log(log('experimentos'))
+.error(log('experimentos'));
 
 const categorias_url = 'https://phet.colorado.edu/es/simulations';
 const categorias = osmosis(categorias_url)
@@ -73,39 +88,14 @@ const categorias = osmosis(categorias_url)
 .log(log('categorias'))
 .error(log('categorias'));
 
-const experimentos = Promise.all([experimentos_descargas, experimentos_data].map(all_data))
-                            .then(unificar_experimentos);
+const experimentos_descargados = all_data(experimentos)
+.then(use(create_dirs))
+.then(use(download_data));
 
-Promise.all([experimentos, all_data(categorias)])
-.then(([experimentos, categorias]) => ({ experimentos, categorias }))
+Promise.all([
+  experimentos_descargados,
+  all_data(categorias)
+])
+.then(([experimentos, categorias]) => ({experimentos, categorias}))
 .then(write_json)
 .catch(log('ERROR'));
-
-//
-// Esto no funciona, lo comento por si en alg�n momento puedo hacerlo andar
-//
-//const actividades_parse_table = (context, data, next) => {
-//  context
-//  .find('tr')
-//  .set({
-//    'key': 'td:first',
-//    'value': 'td:last'
-//  })
-//  .data(({key, value}) => data[key] = value)
-//  .done(() => next(context, data));
-//};
-//
-//const actividades_url = 'https://phet.colorado.edu/es/teaching-resources/browse-activities?sims=all&types=all&levels=all&locales=es&locales=es_CO&locales=es_CR&locales=es_ES&locales=es_MX&locales=es_PE';
-//const actividades = osmosis(actividades_url)
-//.find('a')
-//.set('link', '@href')
-//.follow('@href')
-//.find('table:first')
-//.then(actividades_parse_table)
-//.set({
-//  autor: osmosis.find('table:last').then(actividades_parse_table)
-//})
-//.log(log('actividades'))
-//.error(log('actividades'));
-//
-//all_data(actividades).then((a) => JSON.stringify(a, null, 4)).then(console.log);
